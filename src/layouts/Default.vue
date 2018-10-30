785c34eea32d67a03b1d9e93c6ef5f3ba87bc623
sajid ijaz <sajid.ijaz@zameen.com>
2018-10-30 15:33:00 +0500

    changes of dialog

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,19 +1,26 @@
 <template>
     <section>
+        <HeaderBar />
         <Navbar />
             <section class="main-content pa-3">
                 <slot />
             </section>
         <Footer />
+        <Dialog />
     </section>
 </template>
 
 <script>
     import Navbar from '@/components/Navbar'
     import Footer from '@/components/Footer'
+    import HeaderBar from '@/components/HeaderBar'
+    import Dialog from '@/components/Dialog';
     export default {
         name: "Default",
-        components:{Navbar, Footer}
+        components:{Navbar, Footer, HeaderBar, Dialog},
+        mounted(){
+
+        }
     }
 </script>
 
